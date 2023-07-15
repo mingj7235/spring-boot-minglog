d0b77f02fca21eb7cac57c7a05ef539f2ecea4ba
joshua <joshuara7235@gmail.com>
2023-07-15 16:06:43 +0900

    project init

---
+++ src/main/java/com/minglog/SpringBootMinglogApplication.java
@@ -0,0 +1,13 @@
+package com.minglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMinglogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMinglogApplication.class, args);
+    }
+
+}
