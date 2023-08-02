af98482a95417c6d03ba25654f30a4978dc0b59a
mingj7235 <joshuara7235@gmail.com>
2023-08-02 23:29:43 +0900

    feat : 기본 vue js 컴포넌트 정리
    feat : element plus, bootstrap css import
    
    feat : 글 작성 화면 기본 구축

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+</script>
+
+<template>
+  <div>
+    <el-input type="text" placeholder="제목을 입력해주세요"/>
+  </div>
+
+  <div class="mt-2">
+    <el-input type="textarea" rows="15"/>
+  </div>
+
+  <div class="mt-2">
+    <el-button type="primary"> 글 작성완료 </el-button>
+  </div>
+</template>
+
+<style>
+
+</style>
