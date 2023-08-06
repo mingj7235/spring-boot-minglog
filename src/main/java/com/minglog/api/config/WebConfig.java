d9394dbc842d7ae032c3784b3317d5194a76d4e2
mingj7235 <joshuara7235@gmail.com>
2023-08-06 23:28:06 +0900

    feat : ArgumentResolver 를 통한 인증 로직 구현
     (issue #27)

--- src/main/java/com/minglog/api/config/WebConfig.java
+++ src/main/java/com/minglog/api/config/WebConfig.java
@@ -2,20 +2,21 @@ package com.minglog.api.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    private final AuthInterceptor authInterceptor;
+    private final AuthResolver authResolver;
 
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor)
-                .excludePathPatterns("/foo", "/favicon.ico", "inpageSol.js");
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authResolver);
     }
 
     @Override
