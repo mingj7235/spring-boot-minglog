cff459652c439f41d8205dcbf4478b04f9b3287a
mingj7235 <joshuara7235@gmail.com>
2023-08-10 21:09:11 +0900

    feat : 세션 토큰 인증을 DB 를 통해 인증한다.  (issue #34)

--- src/main/java/com/minglog/api/config/AuthResolver.java
+++ src/main/java/com/minglog/api/config/AuthResolver.java
@@ -1,7 +1,10 @@
 package com.minglog.api.config;
 
 import com.minglog.api.config.data.UserSession;
+import com.minglog.api.domain.Session;
 import com.minglog.api.exception.Unauthorized;
+import com.minglog.api.repository.SessionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -10,7 +13,10 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 @Configuration
+@RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
+
+    private final SessionRepository sessionRepository;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -25,8 +31,9 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
             throw new Unauthorized();
         }
 
-        // DB 에서 토큰과 사용자 확인 작업
+        Session session = sessionRepository.findByAccessToken(accessToken)
+                .orElseThrow(Unauthorized::new);
 
-        return new UserSession(1L);
+        return new UserSession(session.getUser().getId());
     }
 }
