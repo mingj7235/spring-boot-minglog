a482a824a12f92f2e460dccd7df32900909bef00
mingj7235 <joshuara7235@gmail.com>
2023-09-18 21:53:20 +0900

    feat : UserDetails 를 통해 DB 에 저장된 User 로 회원 가입 및 로그인

--- src/main/java/com/minglog/api/config/SecurityConfig.java
+++ src/main/java/com/minglog/api/config/SecurityConfig.java
@@ -1,20 +1,19 @@
 package com.minglog.api.config;
 
+import com.minglog.api.domain.User;
+import com.minglog.api.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 @EnableWebSecurity(debug = true)
@@ -33,7 +32,7 @@ public class SecurityConfig {
 
         return http
                 .authorizeHttpRequests()
-                    .requestMatchers("/auth/login").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/signup").permitAll()
                     .anyRequest().authenticated()
                 .and()
                 .formLogin()
@@ -47,25 +46,22 @@ public class SecurityConfig {
                         .alwaysRemember(false)
                         .tokenValiditySeconds(2592000)
                 )
-                .userDetailsService(userDetailsService())
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        UserDetails user = User
-                .withUsername("joshua")
-                .password("1234")
-                .roles("ADMIN")
-                .build();
-        manager.createUser(user);
-        return manager;
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+
+        return username -> {
+            User user = userRepository.findByEmail(username)
+                    .orElseThrow(() -> new UsernameNotFoundException(username + "을 찾을 수 없습니다."));
+            return new UserPrincipal(user);
+        };
     }
 
     @Bean
     public PasswordEncoder passwordEncoder () {
-        return NoOpPasswordEncoder.getInstance();
+        return new SCryptPasswordEncoder(32, 8, 1, 32, 64);
     }
 }
---
+++ src/main/java/com/minglog/api/config/UserPrincipal.java
@@ -0,0 +1,20 @@
+package com.minglog.api.config;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+public class UserPrincipal extends User {
+
+    private final Long userId;
+
+    public UserPrincipal(com.minglog.api.domain.User user) {
+        super(user.getEmail(), user.getPassword(), List.of(new SimpleGrantedAuthority("ADMIN")));
+        this.userId = user.getId();
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+}
